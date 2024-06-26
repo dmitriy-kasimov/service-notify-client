bb6449275f8e0fa1f75364bdee579071fee9fa28
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2024-06-26 20:15:32 +0300

    add the feature with Modal

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,20 @@ alt.onServer('any:ServiceNotify:c:showAlert', (title: string,
                                body: string,
                                type: string,
                                showTime: number,) => {
-
     browserNotif.emit("c:f:showAlert", title, body, type, showTime);
+    browserNotif.focus();
 })
 
 alt.onServer('any:ServiceNotify:c:showNotification', (id: string,
                                           title: string,
                                           body: string,
                                           showTime: number,) => {
-
     browserNotif.emit("c:f:showNotification", id, title, body, showTime);
+    browserNotif.focus();
+})
+
+alt.onServer('any:ServiceNotify:c:showModalInfo', (title: string,
+                                                      body: string) => {
+    browserNotif.emit("c:f:showModalInfo", title, body);
+    browserNotif.focus();
 })
