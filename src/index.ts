5b6309fb863b10a3fd95a2202a4d2f073d01f89b
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-28 21:38:18 +0300

    refactored all handlers remove unused handler (any:service-notify:c:modal) fixed each other

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,13 @@
-import * as alt from 'alt-client';
-import * as native from 'natives';
-import {WebView} from "alt-client";
+import {WebView, onServer} from "alt-client";
 
-let browserNotif: WebView = new alt.WebView('http://resource/frontend/index.html');
+let browser: WebView = new WebView('http://resource/frontend/index.html');
 
-alt.onServer('any:ServiceNotify:c:showAlert', (title: string,
-                               body: string,
-                               type: string,
-                               showTime: number,) => {
-    browserNotif.emit("c:f:showAlert", title, body, type, showTime);
-    browserNotif.focus();
+onServer('any:service-notify:c:alert', (message: string) => {
+    browser.emit("c:f:alert", message);
+    browser.focus();
 })
 
-alt.onServer('any:ServiceNotify:c:showNotification', (id: string,
-                                          title: string,
-                                          body: string,
-                                          showTime: number,) => {
-    browserNotif.emit("c:f:showNotification", id, title, body, showTime);
-    browserNotif.focus();
-})
-
-alt.onServer('any:ServiceNotify:c:showModalInfo', (title: string,
-                                                      body: string) => {
-    browserNotif.emit("c:f:showModalInfo", title, body);
-    browserNotif.focus();
+onServer('any:service-notify:c:notification', (message: string) => {
+    browser.emit("c:f:notification", message);
+    browser.focus();
 })
